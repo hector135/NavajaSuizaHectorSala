29ac084bf2c918fd11d6bc358a1090addcf390d4
Hector <hector_135_@hotmail.com>
2017-03-10 21:55:17 +0100

    Ultimas optimizaciones. PROYECTO FINAL.

--- NavajaHectorSala/Principal.cs
+++ NavajaHectorSala/Principal.cs
@@ -30,6 +30,7 @@ namespace NavajaHectorSala
         /// <param name="e">Si uso.</param>
         private void button1_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("Introduce un numero y devuelve su factorial. Por ejemplo: introduces 5. 5*4*3*2*1=120");
             NavajaHectorSala.Factorial.Factorial Formulario = new NavajaHectorSala.Factorial.Factorial();
             Formulario.ShowDialog();
         }
@@ -41,6 +42,7 @@ namespace NavajaHectorSala
         /// <param name="e">Si uso.</param>
         private void button2_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("Introduces un numero y te devuelve los numeros de la sucesion de fibonacci tantas veces como el numero que has introducido. Por ejemplo: Introduces 5. 1+1+2+3+5");
             NavajaHectorSala.Fibonacci.Fibonacci Formulario = new NavajaHectorSala.Fibonacci.Fibonacci();
             Formulario.ShowDialog();
         }
@@ -52,6 +54,7 @@ namespace NavajaHectorSala
         /// <param name="e">Si uso.</param>
         private void button3_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("Introduces dos numeros y te devuelve los primos que se encuentran dentro de ese rango. Por ejemplo: Introduces 5 y 10. 5, 7");
             NavajaHectorSala.Primos.Primos Formulario = new NavajaHectorSala.Primos.Primos();
             Formulario.ShowDialog();
         }
@@ -63,6 +66,7 @@ namespace NavajaHectorSala
         /// <param name="e">Si uso.</param>
         private void button4_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("Introduces una frase y te devuelve si es palindroma o no. Por ejemplo: Introduces abccba. Palindromica.");
             NavajaHectorSala.Palindromo.Palindromo Formulario = new NavajaHectorSala.Palindromo.Palindromo();
             Formulario.ShowDialog();
         }
